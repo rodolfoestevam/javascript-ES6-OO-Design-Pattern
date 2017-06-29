dcca47fb1c03de33a0fc7cd5095d18c7e1cabfba
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-06-29 13:09:29 -0300

    object freeze, let, alteracao do prefixo da classe Negociacao

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,9 +1,25 @@
 class Negociacao {
 
-    constructor() {
+    constructor(data, quantidade, valor) {
 
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
     }
 }
